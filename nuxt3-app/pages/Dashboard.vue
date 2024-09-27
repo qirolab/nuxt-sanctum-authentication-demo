11fa456b3a726fcd6b449ca2c08ee8339426a9b3
Harish Kumar <harish.pra22@gmail.com>
2024-09-28 02:16:54 +0530

    feat: add titles to pages

--- nuxt3-app/pages/Dashboard.vue
+++ nuxt3-app/pages/Dashboard.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
+useSeoMeta({
+    title: 'Dashboard',
+})
+
 definePageMeta({
     middleware: ['$auth'],
 });
 </script>
 
 <template>
-    <div class="h-screen flex items-center justify-center">
+    <div class="flex items-center justify-center h-screen">
         <h1 class="text-3xl font-bold">Dashboard</h1>
     </div>
 </template>
--- nuxt3-app/pages/index.vue
+++ nuxt3-app/pages/index.vue
@@ -1,5 +1,11 @@
+<script setup lang="ts">
+useSeoMeta({
+    title: 'Home',
+})
+</script>
+
 <template>
-    <div class="h-screen flex items-center justify-center">
+    <div class="flex items-center justify-center h-screen">
         <h1 class="text-3xl font-bold">Home Page</h1>
     </div>
 </template>
