8be81cab43889f087f52387dc571d52e5da44d80
Harish Kumar <harish.pra22@gmail.com>
2024-10-28 23:38:58 +0530

    feat: Add profile update route

--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -6,3 +6,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::patch('/profile', function (Request $request) {
+    $request->validate([
+        'name' => ['required', 'string', 'max:255'],
+        'email' => ['required', 'email'],
+        'avatar' => [
+            // ['required'],
+            'nullable',
+            'image',
+            'mimes:jpg,png',
+        ],
+    ]);
+
+    $path = null;
+    if ($request->hasFile('avatar')) {
+        $path = $request->file('avatar')->store('avatars', 'public');
+    }
+
+    $request->user()->update([...$request->only('name', 'email'), 'avatar' => $path]);
+
+    return $request->user()->refresh();
+})->middleware(['auth:sanctum']);
